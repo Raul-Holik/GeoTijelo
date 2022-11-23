ba0667d124a81b5a47c85e1ee146b3b6ba7569bc
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2022-11-23 08:51:21 +0100

    Add files via upload

---
+++ GeoTijelo/GeoTijelo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoTijelo
+{
+    class GeoTijelo
+    {
+        double volumen, oplosje;
+        public static int BrojacTijela = 0;
+        
+    }
+    class Kugla:GeoTijelo
+    {
+        static Kugla()
+        {
+            BrojacTijela++;
+        }
+    }
+    class Kvadar : GeoTijelo
+    {
+        static Kvadar()
+        {
+            BrojacTijela++;
+        }
+    }
+    class Kocka:Kvadar
+    {
+        static Kocka()
+        {
+            BrojacTijela++;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kugla x=new Kugla();
+            Kvadar y=new Kvadar();
+            Kocka z=new Kocka();
+
+            Console.WriteLine("Ima "+GeoTijelo.BrojacTijela+" tijela.");
+            
+
+            Console.ReadKey();
+        }
+    }
+}
